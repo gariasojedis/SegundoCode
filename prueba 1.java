cbc56135fab7e9b0704929a5e38db5c5e9ea33cd
gariasojedis <gariasojedis@gmail.com>
2024-01-19 13:42:05 -0500

    cambio 3

--- prueba 1.java
+++ prueba 1.java
@@ -1,20 +1,25 @@
 //************************
 // Archivo: DateExample.java
+// gabriel arias trabajando en cambios 
 //************************
  
 import java.text.*;
 import java.util.*;
+
 import java.util.Date;
  
 public class DateExample {
  
    public static void main(String args[]) {
+
  
      // Obtener la fecha
      Date now = new Date();
+
  
      // Obtener los formateadores de fecha de los entornos nacionales
      // predeterminados, alemán y francés
+     
      DateFormat theDate = DateFormat.getDateInstance(DateFormat.LONG);
      DateFormat germanDate = DateFormat.getDateInstance(DateFormat.LONG, Locale.GERMANY);
      DateFormat frenchDate = DateFormat.getDateInstance(DateFormat.LONG, Locale.FRANCE);
